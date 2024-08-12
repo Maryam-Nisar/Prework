3873ed375ccf889bd29375025b68939e331aff50
Maryam-Nisar <maryamamad0105@gmail.com>
2024-08-12 14:19:21 -0400

    This is the updated files

--- src/pages/EditCreator.jsx
+++ src/pages/EditCreator.jsx
@@ -75,19 +75,25 @@ function EditCreator() {
         </label>
         <h2>Social Media Links</h2>
         <div className="social-media-links">
-          <label>
-            <img src="https://static-00.iconduck.com/assets.00/youtube-icon-2048x2048-oa03jx3h.png" alt="YouTube" />
-            YouTube:
+        <label>
+            <div className="logo-name">
+              <img src="https://static-00.iconduck.com/assets.00/youtube-icon-2048x2048-oa03jx3h.png" alt="YouTube" />
+              <span className="name">YouTube</span>
+            </div>
             <input type="text" name="youtube" value={creator.youtube} onChange={handleChange} />
           </label>
           <label>
-            <img src="https://www.iconpacks.net/icons/2/free-twitter-logo-icon-2429-thumb.png" alt="Twitter" />
-            Twitter:
+            <div className="logo-name">
+              <img src="https://www.iconpacks.net/icons/2/free-twitter-logo-icon-2429-thumb.png" alt="Twitter" />
+              <span className="name">Twitter</span>
+            </div>
             <input type="text" name="twitter" value={creator.twitter} onChange={handleChange} />
           </label>
           <label>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" alt="Instagram" />
-            Instagram:
+            <div className="logo-name">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" alt="Instagram" />
+              <span className="name">Instagram</span>
+            </div>
             <input type="text" name="instagram" value={creator.instagram} onChange={handleChange} />
           </label>
         </div>
